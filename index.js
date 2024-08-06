60084fcdc0c042d4c60a0ff5fd5b99506efb81f1
Roman Gorev <ogogorev@yandex.ru>
2024-08-06 22:32:06 +0200

    Commit current state

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const W = window.innerWidth;
+const H = window.innerHeight;
+
 async function loadFont() {
   const font = new FontFace(
     "Sankofa Display",
@@ -7,16 +10,40 @@ async function loadFont() {
   return font.load();
 }
 
-async function main() {
-  console.log({ w: window.innerWidth, k: window.innerHeight });
+function drawRect(ctx, color, x, y, w, h) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, w, h);
+}
 
-  const cnv = document.querySelector("#canvas");
+const DOT_WIDTH = 4;
 
-  cnv.width = window.innerWidth;
-  cnv.height = window.innerHeight;
+function drawDot(ctx, color, x, y) {
+  drawRect(ctx, color, x, y, DOT_WIDTH, DOT_WIDTH);
+}
 
-  const ctx = cnv.getContext("2d");
+let mousePositionX;
+let mousePositionY;
 
+function addListeners(canvas) {
+  canvas.addEventListener("mousedown", (event) => {
+    mousePositionX = event.offsetX;
+    mousePositionY = event.offsetY;
+  });
+
+  canvas.addEventListener("mousemove", (event) => {
+    if (mousePositionX) {
+      mousePositionX = event.offsetX;
+      mousePositionY = event.offsetY;
+    }
+  });
+
+  canvas.addEventListener("mouseup", (event) => {
+    mousePositionX = undefined;
+    mousePositionY = undefined;
+  });
+}
+
+async function drawInitialState(ctx) {
   ctx.beginPath();
   ctx.rect(0, 0, 150, 100);
   ctx.fill();
@@ -44,6 +71,79 @@ async function main() {
   ctx.fill(canvasPath);
 
   console.log("path", { p, canvasPath });
+
+  return canvasPath;
+}
+
+async function main() {
+  console.log({ w: window.innerWidth, k: window.innerHeight });
+
+  const cnv = document.querySelector("#canvas");
+
+  cnv.width = W;
+  cnv.height = H;
+
+  const ctx = cnv.getContext("2d");
+
+  const offscreenCanvas = new OffscreenCanvas(W, H);
+  const offscreenContext = offscreenCanvas.getContext("2d");
+
+  const canvasPath = await drawInitialState(offscreenContext);
+
+  /// ==========
+
+  let rectX = 200;
+  let rectY = 200;
+  const RECT_W = 50;
+  const RECT_H = 50;
+
+  let start = 0;
+  let elapsed = 0;
+
+  addListeners(canvas);
+
+  function draw(timestamp) {
+    console.log(timestamp);
+
+    ctx.reset();
+
+    ctx.drawImage(offscreenCanvas, 0, 0);
+
+    const d = timestamp - start;
+
+    rectX = rectX + 1;
+
+    let scaleX = 1;
+    let scaleY = 1;
+
+    if (mousePositionX) {
+      scaleX = mousePositionX / 300;
+      scaleY = mousePositionY / 300;
+    }
+
+    ctx.save();
+
+    ctx.scale(scaleX, scaleY);
+
+    ctx.beginPath();
+
+    // drawRect(ctx, "blue", mousePositionX, mousePositionY, RECT_W, RECT_H);
+    // ctx.translate(mousePositionX, mousePositionY);
+    ctx.fill(canvasPath);
+
+    // ctx.closePath();
+
+    ctx.restore();
+
+    start = timestamp;
+    elapsed += d;
+
+    if (elapsed < 20000) {
+      window.requestAnimationFrame(draw);
+    }
+  }
+
+  window.requestAnimationFrame(draw);
 }
 
 window.onload = () => {
@@ -51,3 +151,30 @@ window.onload = () => {
 };
 
 // main();
+
+///// ==========
+
+//   function draw(timestamp) {
+//     console.log(timestamp);
+
+//     ctx.reset();
+
+//     ctx.drawImage(offscreenCanvas, 0, 0);
+
+//     const d = timestamp - start;
+
+//     rectX = rectX + 1;
+
+//     ctx.beginPath();
+//     drawRect(ctx, "blue", mousePositionX, mousePositionY, RECT_W, RECT_H);
+//     ctx.translate(mousePositionX, mousePositionY);
+//     ctx.fill(canvasPath);
+//     ctx.closePath();
+
+//     start = timestamp;
+//     elapsed += d;
+
+//     if (elapsed < 20000) {
+//       window.requestAnimationFrame(draw);
+//     }
+//   }
