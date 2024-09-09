2e2c41235022829fbbf342f6ec0ca0940730dc9d
Roman Gorev <ogogorev@yandex.ru>
2024-09-09 23:56:25 +0200

    Store fonts as ttf, add Roboto font

--- src/components/Editor/Core/fonts.js
+++ src/components/Editor/Core/fonts.js
@@ -1,45 +1,34 @@
 import opentype from "opentype.js";
-// import wawoff2 from "wawoff2";
+
+import robotoUrl from "/src/assets/fonts/Roboto-Regular.ttf";
+import sankofaDisplayUrl from "/src/assets/fonts/SankofaDisplay-Regular.ttf";
 
 export const FONTS = {
   SankofaDisplay: {
     id: "SankofaDisplay",
     name: "Sankofa Display",
-    // "url('https://fonts.googleapis.com/css2?family=Sankofa+Display&display=swap')"
-    // "url(https://fonts.gstatic.com/s/sankofadisplay/v2/Ktk1ALSRd4LucUDghJ2rTqXOoh3HEKOYd4xI5g.woff2) format('woff2')"
-    url: "https://fonts.gstatic.com/s/sankofadisplay/v2/Ktk1ALSRd4LucUDghJ2rTqXOoh3HEKOYd4xI5g.woff2",
-    // url: "/fonts/Roboto-Regular.ttf",
+    url: sankofaDisplayUrl,
+  },
+  Roboto: {
+    id: "Roboto",
+    name: "Roboto",
+    url: robotoUrl,
   },
 };
 
 export const loadedFonts = {};
 
-// export async function loadFont(fontInfo) {
-//   const res = await fetch(fontInfo.url);
-//   const fontBuffer = res.arrayBuffer();
-
-//   console.log({ fontBuffer });
-
-//   const font = opentype.parse(await fontBuffer);
-
-//   loadedFonts[fontInfo.id] = font;
-// }
-
 export async function loadFont(fontInfo) {
-  try {
-    const res = await fetch(fontInfo.url);
-    const fontBuffer = res.arrayBuffer();
+  const res = await fetch(fontInfo.url);
+  const fontBuffer = res.arrayBuffer();
 
-    const font = await fontBuffer
-      .then((buffer) => Module.decompress(buffer))
-      .then((buffer) => opentype.parse(Uint8Array.from(buffer).buffer));
+  console.log({ fontBuffer });
 
-    loadedFonts[fontInfo.id] = font;
-  } catch (e) {
-    console.error(e);
-  }
+  const font = opentype.parse(await fontBuffer);
+
+  loadedFonts[fontInfo.id] = font;
 }
 
 export async function loadFonts() {
-  await loadFont(FONTS.SankofaDisplay);
+  await Promise.all(Object.values(FONTS).map(loadFont));
 }
